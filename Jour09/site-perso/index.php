e9cd3612bdc231cbc5e54b658f85e53a11606975
viktor-argirov <viktor.argirov@laplateforme.io>
2023-07-26 14:14:44 +0200

    Site-persoé

---
+++ Jour09/site-perso/index.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Viktor Argirov - Développeur web</title>
+  <meta name="description" content="Viktor Argirov - Développeur web, étudiant à LaPlateforme_  Cannes.">
+  <link  rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+  <link rel="stylesheet" href="style/sass/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css">
+</head>
+
+
+<body>
+<main>
+
+    <header>
+
+      <div id="div_logo">
+        <a href="index.php"><img src="medias/logo/logo.jpg" alt="logo viktor argirov"></a>
+        <h1>Viktor Argirov</h1>
+        <p>Développeur web</p>
+      </div>
+
+      <div id="nav_et_rs">
+        <nav>
+          <button id="premier_boutton" class="lien_nav">à propos</button>
+          <button id="deuxieme_boutton" class="lien_nav">compétences</button>
+          <button id="troisieme_boutton" class="lien_nav">porfolio</button>
+          <button id="dernier_boutton" class="lien_nav">contact</button>
+        </nav>
+
+        <div id="div_reseaux_sociaux">
+          <a target="_blank" href="https://www.linkedin.com/in/viktor-argirov-24b122272"><img src="medias/rs/031-linkedin.png" alt="logo linkedin"></a>
+          <a target="_blank" href="https://github.com/viktor-argirov"><img src="medias/rs/039-github.png" alt="logo github"></a>
+          <!-- <a href=""><img src="medias/rs/022-skype.png" alt="logo skype"></a> -->
+        </div>
+      </div>
+
+    </header>
+
+
+
+    <section id="section_centrale">
+
+      <div id="accueil">
+
+        <div id="container">
+            <div id="para_1">
+              <p>à propos</p>
+            </div>
+            <div id="para_2">
+              <p>compétences</p>
+            </div>
+            <div id="para_3">
+              <p>portfolio</p>
+            </div>
+            <div id="para_4">
+              <p>contact</p>
+            </div>
+        </div>
+
+        <div class="type-js headline">
+          <p id="text_hover1" class="text-js">Bonjour</p>
+        </div>
+        <div class="type-js2 headline">
+          <p id="text_hover2" class="text-js">Je suis Viktor,</p>
+        </div>
+        <div class="type-js3 headline">
+          <p id="text_hover3" class="text-js">étudiant en développement web</p>
+        </div>
+
+        <p id="details_dev" class="animate__animated animate__fadeInUp">Développeur Full Stack</p>
+
+      </div>
+        
+
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</main>
+
+  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
+<!-- <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+<script src='script.js'></script>
+</body>
+</html>
